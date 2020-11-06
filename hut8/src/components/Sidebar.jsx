271470085bb85e3c91920e3530289b920312cb4a
Marcelo <mcribeiro@inf.ufpel.edu.br>
2020-11-06 19:08:21 -0300

    edits to sidebar

--- hut8/src/components/Sidebar.jsx
+++ hut8/src/components/Sidebar.jsx
@@ -2,7 +2,15 @@ import '../styles/components/sidebar.css'
 function Sidebar(){
 
     return (
-        <div className="sidebar">asdasd</div>
+        <div className="sidebar">
+            <img src="https://i.imgur.com/aU48TWI.jpg" className="icon" alt="OLOCO BIXO"/>
+            <h2 className="user-name">Fausto Silva</h2>
+            <p>Ciência da Computação</p>
+
+            <h1 className="dashboard">Dashboard</h1>
+            <h1 className="courses">Meus cursos</h1>
+
+        </div>
     )
 }
 
